7af0990897c6b1c048fe258c96a9cd6aedacd0c0
a145789 <2091927351@qq.com>
2023-04-21 14:03:25 +0800

    feat: add article

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,4 +1,53 @@
+import clsx from "clsx"
 import Head from "next/head"
+
+const theme = [
+  {
+    primary: "bg-#F44336",
+    color: "text-#FFFFFF",
+    border: "border-#D32F2F",
+  },
+  {
+    primary: "bg-#FFC107",
+    color: "text-#FFFFFF",
+    border: "border-#FFA000",
+  },
+  {
+    primary: "bg-#4CAF50",
+    color: "text-#FFFFFF",
+    border: "border-#388E3C",
+  },
+  {
+    primary: "bg-#2196F3",
+    color: "text-#FFFFFF",
+    border: "border-#1976D2",
+  },
+  {
+    primary: "bg-#E91E63",
+    color: "text-#FFFFFF",
+    border: "border-#C2185B",
+  },
+  {
+    primary: "bg-#212121",
+    color: "text-#FFFFFF",
+    border: "border-#757575",
+  },
+] as const
+const label = [
+  "前端工程师",
+  "Vue 小能手",
+  "不怎么喜欢 React 但又能怎么办呢",
+  "TypeScript 666",
+  "Node.js 写脚本的福音",
+  "Vite 是真的快啊",
+  "快乐的风女，听候您的差遣",
+  "与光同行者",
+  "不要让懒惰吞噬你",
+  "间歇性 emo",
+  "你不在了我可怎么活",
+  "开玩笑的",
+  "嘻嘻",
+] as const
 
 export default function About() {
   return (
@@ -6,8 +55,25 @@ export default function About() {
       <Head>
         <title>About</title>
       </Head>
-      <div className="w-full h-full flex flex-col justify-center items-center font-900 text-60px">
-        前端程序员
+      <div className="mt-24 mx-12">
+        <span>标签：</span>
+        {label.map((item, index) => {
+          const { primary, color, border } =
+            index < theme.length ? theme[index] : theme[index % theme.length]
+          return (
+            <span
+              className={clsx(
+                "inline-block rounded-2px px-8px py-5px text-14px mr-2 mb-2",
+                primary,
+                color,
+                border
+              )}
+              key={item}
+            >
+              {item}
+            </span>
+          )
+        })}
       </div>
     </>
   )
