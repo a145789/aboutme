e270b0584d5a1df34b3c72dc6e05663818df3878
a145789 <37403253+a145789@users.noreply.github.com>
2023-07-31 06:12:17 +0000

    Deploying to gh-pages from @ a145789/aboutme@4cb5b89bb342f66035926cbfeac44ef9e6a30143 🚀

---
+++ _next/static/chunks/pages/_error-887b1ba26b408cea.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{4637:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(5367)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=4637)}),_N_E=n.O()}]);
